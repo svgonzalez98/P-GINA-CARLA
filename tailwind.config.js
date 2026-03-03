2a2f367fa2085a975b3e1b46c73072d25a0bfca0
sgonzalez <sgonzales@skypostal.com>
2026-03-02 19:00:34 -0500

    fix code - pfc

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./index.html", "./pages/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'pine-dark': '#0E2F26',
+        'pine': '#12372C',
+        'pine-light': '#1a4035',
+        'pine-lighter': '#2a5245',
+        'sage': '#C9A35C',
+        'sage-light': '#B88A3C',
+        'cool-bg': '#EAF3EE',
+      },
+      fontFamily: {
+        'sans': ['Inter', 'sans-serif'],
+        'serif': ['Cormorant Garamond', 'serif'],
+      }
+    }
+  }
+}
